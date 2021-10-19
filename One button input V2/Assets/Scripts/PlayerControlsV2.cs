2f07754fe6d6982c232a6b407386342672d7ff83
AlyxLion <MagentaLionAlyx@gmail.com>
2021-10-19 21:56:36 +1100

    Change to the cannons so they turn and face mouse

--- One button input V2/Assets/Scripts/PlayerControlsV2.cs
+++ One button input V2/Assets/Scripts/PlayerControlsV2.cs
@@ -7,7 +7,9 @@ public class PlayerControlsV2 : MonoBehaviour
     
     public float speed;
     public GameObject bullet;
-    
+    public bool right;
+
+
     void Start()
     {
         //Cursor.lockState = CursorLockMode.Locked;
@@ -24,8 +26,23 @@ public class PlayerControlsV2 : MonoBehaviour
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);*/
 
         //Test two of using Input.MousePosition I hope the two towers will shoot at the mouse
-        transform.LookAt(Input.mousePosition, Camera.main.ScreenToWorldPoint(transform.position));
-        
+        //transform.LookAt(Input.mousePosition, Camera.main.ScreenToWorldPoint(transform.position)); failed
+
+        Vector3 turnTowards = Input.mousePosition;
+        Quaternion lookRotation = Quaternion.identity;
+
+        if (!right)
+        {
+            Vector3 direction = (turnTowards - transform.position).normalized;
+            lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y));
+        }
+        else
+        {
+            Vector3 direction = (-turnTowards - transform.position).normalized;
+            lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, -direction.y));
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * speed);
+
 
         //can shoot
         if (Input.GetButtonDown("Fire1"))
